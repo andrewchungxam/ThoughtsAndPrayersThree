4acaf9ed4172bd79b69781f6bee6082eea999de7
ac <andrew.chung@xamarin.com>
2017-11-07 19:33:23 -0800

    simple_grid working
    simple grid working

--- ThoughtsAndPrayersThree/ThoughtsAndPrayersThree/Pages/ViewCells/PrayerViewCell.cs
+++ ThoughtsAndPrayersThree/ThoughtsAndPrayersThree/Pages/ViewCells/PrayerViewCell.cs
@@ -129,64 +129,144 @@ namespace ThoughtsAndPrayersThree.Pages.ViewCells
             prayerButton.SetBinding(Button.CommandParameterProperty, new Binding("."));
             prayerButton.SetBinding(Button.CommandProperty, new Binding("BindingContext.PrayerClickCommand", source: prayerListPage));
 
-            //            thoughtButton.SetBinding(Button.BindingContextProperty, new Binding("."));
-            //         thoughtButton.SetBinding(Button.BindingContextProperty, new Binding("."));
-            ////thoughtButton.SetBinding(Button.CommandParameterProperty, new Binding("."));
-            //thoughtButton.SetBinding(Button.TextProperty, "ButtonText" );
-            //thoughtButton.SetBinding(Button.CommandProperty, new Binding(nameof(ViewModels.PrayerListViewModel.ThoughtClickCommand)));
+			//            thoughtButton.SetBinding(Button.BindingContextProperty, new Binding("."));
+			//         thoughtButton.SetBinding(Button.BindingContextProperty, new Binding("."));
+			////thoughtButton.SetBinding(Button.CommandParameterProperty, new Binding("."));
+			//thoughtButton.SetBinding(Button.TextProperty, "ButtonText" );
+			//thoughtButton.SetBinding(Button.CommandProperty, new Binding(nameof(ViewModels.PrayerListViewModel.ThoughtClickCommand)));
 
 
-            //https://forums.xamarin.com/discussion/58709/context-actions-command-binding-with-parameter#latest
+			//https://forums.xamarin.com/discussion/58709/context-actions-command-binding-with-parameter#latest
 
-            //thoughtButton.SetBinding(Button.CommandProperty, nameof(prayerListViewModel.ThoughtClickCommand));
-            //thoughtButton.BindingContext = new Binding(".");
+			//thoughtButton.SetBinding(Button.CommandProperty, nameof(prayerListViewModel.ThoughtClickCommand));
+			//thoughtButton.BindingContext = new Binding(".");
 
-            //_firstButton.SetBinding(Button.CommandProperty, nameof(MyViewModel.MyFavoriteCommand));
+			//_firstButton.SetBinding(Button.CommandProperty, nameof(MyViewModel.MyFavoriteCommand));
 
-            var textStack = new StackLayout
-            {
-                //Padding = 10, //E
-                //HorizontalOptions = LayoutOptions.FillAndExpand, //NE
-                Margin = new Thickness(0, 3, 0, 0),
-                //VerticalOptions = LayoutOptions.FillAndExpand, //NE
-                Orientation = StackOrientation.Vertical,
-                Children =
-                    {
-                        myNameProperty,
-                        dateString,
-                        myPrayerRequestProperty,
-                        thoughtButton,
-                        prayerButton
-                    }
+			/////////////////////////////////////////////////////////////////////////////////////////////////////////
+			/////////////////////
+			/////////OPTION 1 - StackLayout
+			//        var textStack = new StackLayout
+			//        {
+			//            //Padding = 10, //E
+			//            //HorizontalOptions = LayoutOptions.FillAndExpand, //NE
+			//            Margin = new Thickness(0, 3, 0, 0),
+			//            //VerticalOptions = LayoutOptions.FillAndExpand, //NE
+			//            Orientation = StackOrientation.Vertical,
+			//            Children =
+			//                {
+			//                    myNameProperty,
+			//                    dateString,
+			//                    myPrayerRequestProperty,
+			//                    thoughtButton,
+			//                    prayerButton
+			//                }
+			//        };
+
+			//        var cellLayoutStack = new StackLayout
+			//        {
+			//            Margin = new Thickness(0, 0, 0, 0),
+			//            //Spacing = 10, //default is 6
+			//            Orientation = StackOrientation.Horizontal,
+			//            Children =
+			//                {
+			//                    cachedImage,
+			////					userImage,
+			//textStack
+			//        }
+			//};
+
+			//View = cellLayoutStack;
+			/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+			var grid = new Grid();
+
+			//grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(2, GridUnitType.Star) });
+			//grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+			//grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(200) });
+			//grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(200) });
+
+			//grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(200) });
+			//grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(200) });
+			//grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(200) });
+			//grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(200) });
+
+			grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+			grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+			grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }); //, GridUnitType.Star) });
+			grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+
+			grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+			grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+			grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+			grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+			//var topLeft = new Label { Text = "Top Left" };
+			//var topRight = new Label { Text = "Top Right" };
+			//var bottomLeft = new Label { Text = "Bottom Left" };
+			//var bottomRight = new Label { Text = "Bottom Right" };
+
+			//grid.Children.Add(topLeft, 0, 0);
+			//grid.Children.Add(topRight, 0, 1);
+			//grid.Children.Add(bottomLeft, 1, 0);
+			//grid.Children.Add(bottomRight, 1, 1);
+
+
+			var Picture = new Label { Text = "Picture Picture Picture Picture Picture Picture Picture Picture Picture"  };
+			var Name = new Label { Text = "First and Last Name" };
+			var Time = new Label { Text = "Time" };
+			var PrayerRequest = new Label { 
+//                Text = "Text of prayer request. Text of prayer request.  Text of prayer request.  Text of prayer request.  Text of prayer request.   Text of prayer request.  Text of prayer request." 
+                Text = "Text of prayer request."
             };
+			var ThoughtsNumber = new Label { Text = "30 Thoughts" };
+            var PrayerNumber = new Label { Text = "3 Prayer" };
+            var ThoughtsButton = new Label { Text = "Thoughts Button" };
+            var PrayerButton = new Label { Text = "Prayer Button" };
 
-            var cellLayoutStack = new StackLayout
-            {
-                Margin = new Thickness(0, 0, 0, 0),
-                //Spacing = 10, //default is 6
-                Orientation = StackOrientation.Horizontal,
-                Children =
-                    {
-                        cachedImage,
-    //					userImage,
-    					textStack
-                    }
-            };
+			grid.Children.Add(Picture, 0, 0);
+            Grid.SetRowSpan(Picture, 2);
+			grid.Children.Add(Name, 1, 0);
+			Grid.SetColumnSpan(Name, 3);
+			grid.Children.Add(Time, 1, 1);
+            Grid.SetColumnSpan(Time, 3);
 
-            View = cellLayoutStack;
+			grid.Children.Add(PrayerRequest, 0, 2);
+            Grid.SetColumnSpan(PrayerRequest, 4);
+            grid.Children.Add(ThoughtsNumber, 0, 3);
+			Grid.SetColumnSpan(ThoughtsNumber, 2);
+			grid.Children.Add(PrayerNumber, 2, 3);
+			Grid.SetColumnSpan(PrayerNumber, 2);
+            grid.Children.Add(ThoughtsButton, 0, 4);
+			Grid.SetColumnSpan(ThoughtsButton, 2);
+            grid.Children.Add(PrayerButton, 2, 4);
+			Grid.SetColumnSpan(PrayerButton, 2);
+			View = grid;
 
-            //MenuItem ITEM AND CONTEXT ACTIONS
-            //var moreAction = new MenuItem { Text = "More" };
-            //moreAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
-            //moreAction.Clicked += async (sender, e) =>
-            //{
-            //    var mi = ((MenuItem)sender);
-            //    Debug.WriteLine("More Context Action clicked: " + mi.CommandParameter);
-            //};
-            // add to the ViewCell's ContextActions property
-            //ContextActions.Add(moreAction);
 
-            _deleteAction = new MenuItem { Text = "Delete", IsDestructive = true }; // red background
+
+
+
+
+
+
+
+
+
+			//MenuItem ITEM AND CONTEXT ACTIONS
+			//var moreAction = new MenuItem { Text = "More" };
+			//moreAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
+			//moreAction.Clicked += async (sender, e) =>
+			//{
+			//    var mi = ((MenuItem)sender);
+			//    Debug.WriteLine("More Context Action clicked: " + mi.CommandParameter);
+			//};
+			// add to the ViewCell's ContextActions property
+			//ContextActions.Add(moreAction);
+
+			_deleteAction = new MenuItem { Text = "Delete", IsDestructive = true }; // red background
             _deleteAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
 
             //_deleteAction.Clicked += async (sender, e) =>
