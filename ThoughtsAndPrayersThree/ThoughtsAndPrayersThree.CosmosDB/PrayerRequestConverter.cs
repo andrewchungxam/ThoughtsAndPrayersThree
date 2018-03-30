0087aede4e5e53914f6fcb30b4c9497c40d00121
ac <andrew.chung@xamarin.com>
2018-03-30 15:04:49 -0700

    commenting out CosmosDBa
    commenting out CosmosDBa

--- ThoughtsAndPrayersThree/ThoughtsAndPrayersThree.CosmosDB/PrayerRequestConverter.cs
+++ ThoughtsAndPrayersThree/ThoughtsAndPrayersThree.CosmosDB/PrayerRequestConverter.cs
@@ -1,46 +1,46 @@
-﻿using System;
+﻿//using System;
 
-using ThoughtsAndPrayersThree.Models;
-using ThoughtsAndPrayersThree.Models;
+//using ThoughtsAndPrayersThree.Models;
+//using ThoughtsAndPrayersThree.Models;
 
-namespace ThoughtsAndPrayersThree.CosmosDB
-{
-	public static class PrayerRequestConverter
-	{
-		public static CosmosDBPrayerRequest ConvertToCosmosPrayerRequest(PrayerRequest prayerRequest)
-		{
-			var myIdString = (prayerRequest.Id).ToString();
+//namespace ThoughtsAndPrayersThree.CosmosDB
+//{
+//	public static class PrayerRequestConverter
+//	{
+//		public static CosmosDBPrayerRequest ConvertToCosmosPrayerRequest(PrayerRequest prayerRequest)
+//		{
+//			var myIdString = (prayerRequest.Id).ToString();
 
-			var myCosmosDBPrayerRequest = new CosmosDBPrayerRequest()
-			{
-				Id = myIdString,
-				CreatedDateTimeString = prayerRequest.CreatedDateTimeString,
-				CreatedDateTime = prayerRequest.CreatedDateTime,
-				FirstName = prayerRequest.FirstName,
-				LastName = prayerRequest.LastName,
-				FullName = prayerRequest.FullName,
-				FBProfileUrl = prayerRequest.FBProfileUrl,
-				PrayerRequestText = prayerRequest.PrayerRequestText
-			};
-			return myCosmosDBPrayerRequest;
-		}
+//			var myCosmosDBPrayerRequest = new CosmosDBPrayerRequest()
+//			{
+//				Id = myIdString,
+//				CreatedDateTimeString = prayerRequest.CreatedDateTimeString,
+//				CreatedDateTime = prayerRequest.CreatedDateTime,
+//				FirstName = prayerRequest.FirstName,
+//				LastName = prayerRequest.LastName,
+//				FullName = prayerRequest.FullName,
+//				FBProfileUrl = prayerRequest.FBProfileUrl,
+//				PrayerRequestText = prayerRequest.PrayerRequestText
+//			};
+//			return myCosmosDBPrayerRequest;
+//		}
 
-		public static PrayerRequest ConvertToPrayerRequest(CosmosDBPrayerRequest cosmosDBPrayerRequest)
-		{
-			var myIdInt = Int32.Parse(cosmosDBPrayerRequest.Id);
+//		public static PrayerRequest ConvertToPrayerRequest(CosmosDBPrayerRequest cosmosDBPrayerRequest)
+//		{
+//			var myIdInt = Int32.Parse(cosmosDBPrayerRequest.Id);
 
-			var myPrayerRequest = new PrayerRequest()
-			{
-				Id = myIdInt,
-				CreatedDateTimeString = cosmosDBPrayerRequest.CreatedDateTimeString,
-				CreatedDateTime = cosmosDBPrayerRequest.CreatedDateTime,
-				FirstName = cosmosDBPrayerRequest.FirstName,
-				LastName = cosmosDBPrayerRequest.LastName,
-				FullName = cosmosDBPrayerRequest.FullName,
-				FBProfileUrl = cosmosDBPrayerRequest.FBProfileUrl,
-				PrayerRequestText = cosmosDBPrayerRequest.PrayerRequestText
-			};
-			return myPrayerRequest;
-		}
-	}
-}
+//			var myPrayerRequest = new PrayerRequest()
+//			{
+//				Id = myIdInt,
+//				CreatedDateTimeString = cosmosDBPrayerRequest.CreatedDateTimeString,
+//				CreatedDateTime = cosmosDBPrayerRequest.CreatedDateTime,
+//				FirstName = cosmosDBPrayerRequest.FirstName,
+//				LastName = cosmosDBPrayerRequest.LastName,
+//				FullName = cosmosDBPrayerRequest.FullName,
+//				FBProfileUrl = cosmosDBPrayerRequest.FBProfileUrl,
+//				PrayerRequestText = cosmosDBPrayerRequest.PrayerRequestText
+//			};
+//			return myPrayerRequest;
+//		}
+//	}
+//}
--- ThoughtsAndPrayersThree/ThoughtsAndPrayersThree/Constants/ASampleAppConstants.cs
+++ ThoughtsAndPrayersThree/ThoughtsAndPrayersThree/Constants/ASampleAppConstants.cs
@@ -1,27 +1,27 @@
-﻿using System;
-namespace ThoughtsAndPrayersThree.Constants
-	{
-		static public class ASampleAppConstants
-		{
-			public static String MobileCenterIOS { get; } = "\"ios=294968e2-d712-43fc-a8bd-1492ed9ea29c;\"";
-		}
-	}
+﻿//using System;
+//namespace ThoughtsAndPrayersThree.Constants
+	//{
+	//	static public class ASampleAppConstants
+	//	{
+	//		public static String MobileCenterIOS { get; } = "\"ios=294968e2-d712-43fc-a8bd-1492ed9ea29c;\"";
+	//	}
+	//}
 
-	namespace ThoughtsAndPrayersThree.BlobStorage
-	{
-		public static class AzureBlobConstants
-		{
-			public static String BlobUrlAndKey = "DefaultEndpointsProtocol=https;AccountName=asampleappfive;AccountKey=ACCOUNT_KEY==";
-		}
-	}
+	//namespace ThoughtsAndPrayersThree.BlobStorage
+	//{
+	//	public static class AzureBlobConstants
+	//	{
+	//		public static String BlobUrlAndKey = "DefaultEndpointsProtocol=https;AccountName=asampleappfive;AccountKey=ACCOUNT_KEY==";
+	//	}
+	//}
 
-	namespace ThoughtsAndPrayersThree.CosmosDB
-	{
-		public static class CosmosDBStrings
-		{
-			//#error SIGN UP FOR AZURE - THEN SET THE BELOW KEYS AND THEN COMMENT OUT THIS LINE
-			//static readonly String myEndPoint = "https://YOUR_AZURE_COSMODB_INSTANCE.documents.azure.com:44339283123/";
-			//static readonly String myKey = "
-			//#error Make sure to add this Nuget package to all your platform projects Microsoft.Azure.DocumentDb.Core
-		}
-	}
+	//namespace ThoughtsAndPrayersThree.CosmosDB
+	//{
+	//	public static class CosmosDBStrings
+	//	{
+	//		//#error SIGN UP FOR AZURE - THEN SET THE BELOW KEYS AND THEN COMMENT OUT THIS LINE
+	//		//static readonly String myEndPoint = "https://YOUR_AZURE_COSMODB_INSTANCE.documents.azure.com:44339283123/";
+	//		//static readonly String myKey = "
+	//		//#error Make sure to add this Nuget package to all your platform projects Microsoft.Azure.DocumentDb.Core
+	//	}
+	//}
