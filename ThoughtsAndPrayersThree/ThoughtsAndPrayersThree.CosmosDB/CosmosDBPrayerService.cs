c35aee813aadf8e1370fa2b59ea1b87fe8fd0131
ac <andrew.chung@xamarin.com>
2018-03-30 15:06:45 -0700

    commented out
    commented out

--- ThoughtsAndPrayersThree/ThoughtsAndPrayersThree.CosmosDB/CosmosDBPrayerService.cs
+++ ThoughtsAndPrayersThree/ThoughtsAndPrayersThree.CosmosDB/CosmosDBPrayerService.cs
@@ -1,84 +1,84 @@
-﻿using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Diagnostics;
+//using System.Threading.Tasks;
+//using System.Collections.Generic;
 
-using Microsoft.Azure.Documents;
-using Microsoft.Azure.Documents.Linq;
-using Microsoft.Azure.Documents.Client;
+//using Microsoft.Azure.Documents;
+//using Microsoft.Azure.Documents.Linq;
+//using Microsoft.Azure.Documents.Client;
 
-//using ASampleApp;
-//using ASampleApp.Models;
-using ThoughtsAndPrayersThree.Models;
+////using ASampleApp;
+////using ASampleApp.Models;
+//using ThoughtsAndPrayersThree.Models;
 
-namespace ThoughtsAndPrayersThree.CosmosDB
-{
-	public class CosmosDBPrayerService
-	{
-		//DBS - Collections - Documents
-		static readonly string DatabaseId = "Xamarin";
-		static readonly string CollectionId = "PrayerRequests";
+//namespace ThoughtsAndPrayersThree.CosmosDB
+//{
+//	public class CosmosDBPrayerService
+//	{
+//		//DBS - Collections - Documents
+//		static readonly string DatabaseId = "Xamarin";
+//		static readonly string CollectionId = "PrayerRequests";
 
-		//CLIENT
-		static readonly DocumentClient myDocumentClient = new DocumentClient(new Uri(CosmosDB.CosmosDBPrayerRequestStrings.myEndPoint), CosmosDB.CosmosDBPrayerRequestStrings.myKey);
+//		//CLIENT
+//		static readonly DocumentClient myDocumentClient = new DocumentClient(new Uri(CosmosDB.CosmosDBPrayerRequestStrings.myEndPoint), CosmosDB.CosmosDBPrayerRequestStrings.myKey);
 
-		public static List<CosmosDBPrayerRequest> MyListOfCosmosDogs;
+//		public static List<CosmosDBPrayerRequest> MyListOfCosmosDogs;
 
-		//GETALL
-		public static async Task<List<CosmosDBPrayerRequest>> GetAllCosmosPrayerRequests()
-		{
-			MyListOfCosmosDogs = new List<CosmosDBPrayerRequest>();
-			try
-			{
+//		//GETALL
+//		public static async Task<List<CosmosDBPrayerRequest>> GetAllCosmosPrayerRequests()
+//		{
+//			MyListOfCosmosDogs = new List<CosmosDBPrayerRequest>();
+//			try
+//			{
 
-				var query = myDocumentClient.CreateDocumentQuery<CosmosDBPrayerRequest>(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId))
-											.AsDocumentQuery();
-				while (query.HasMoreResults)
-				{
-					MyListOfCosmosDogs.AddRange(await query.ExecuteNextAsync<CosmosDBPrayerRequest>());
-				}
-			}
-			catch (DocumentClientException ex)
-			{
-				Debug.WriteLine("Error: ", ex.Message);
-			}
-			return MyListOfCosmosDogs;
-		}
+//				var query = myDocumentClient.CreateDocumentQuery<CosmosDBPrayerRequest>(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId))
+//											.AsDocumentQuery();
+//				while (query.HasMoreResults)
+//				{
+//					MyListOfCosmosDogs.AddRange(await query.ExecuteNextAsync<CosmosDBPrayerRequest>());
+//				}
+//			}
+//			catch (DocumentClientException ex)
+//			{
+//				Debug.WriteLine("Error: ", ex.Message);
+//			}
+//			return MyListOfCosmosDogs;
+//		}
 
-		//GET
-		public static async Task<List<CosmosDBPrayerRequest>> GetCosmosPrayerRequestsByIdAsync(string id)
-		{
-			var result = await myDocumentClient.ReadDocumentAsync<CosmosDBPrayerRequest>(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id));
+//		//GET
+//		public static async Task<List<CosmosDBPrayerRequest>> GetCosmosPrayerRequestsByIdAsync(string id)
+//		{
+//			var result = await myDocumentClient.ReadDocumentAsync<CosmosDBPrayerRequest>(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id));
 
-			if (result.StatusCode != System.Net.HttpStatusCode.OK)
-			{
-				return null;
-			}
+//			if (result.StatusCode != System.Net.HttpStatusCode.OK)
+//			{
+//				return null;
+//			}
 
-			List<CosmosDBPrayerRequest> returnedListCosmosDog = new List<CosmosDBPrayerRequest>();
-			returnedListCosmosDog.Add(result);
+//			List<CosmosDBPrayerRequest> returnedListCosmosDog = new List<CosmosDBPrayerRequest>();
+//			returnedListCosmosDog.Add(result);
 
-			return returnedListCosmosDog;
+//			return returnedListCosmosDog;
 
-		}
+//		}
 
-		//POST
-		public static async Task PostCosmosPrayerRequestsAsync(CosmosDBPrayerRequest cosmosDBPrayerRequest)
-		{
-			await myDocumentClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId), cosmosDBPrayerRequest);
+//		//POST
+//		public static async Task PostCosmosPrayerRequestsAsync(CosmosDBPrayerRequest cosmosDBPrayerRequest)
+//		{
+//			await myDocumentClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId), cosmosDBPrayerRequest);
 
-		}
+//		}
 
-		//PUT
-		public static async Task PutCosmosPrayerRequestsAsync(CosmosDBPrayerRequest cosmosDBPrayerRequest)
-		{
-			await myDocumentClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, cosmosDBPrayerRequest.Id), cosmosDBPrayerRequest);
-		}
+//		//PUT
+//		public static async Task PutCosmosPrayerRequestsAsync(CosmosDBPrayerRequest cosmosDBPrayerRequest)
+//		{
+//			await myDocumentClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, cosmosDBPrayerRequest.Id), cosmosDBPrayerRequest);
+//		}
 
-		//DELETE
-		public static async Task DeleteCosmosPrayerRequestsAsync(CosmosDBPrayerRequest deleteCosmosDBPrayerRequest)
-		{
-			await myDocumentClient.DeleteDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, deleteCosmosDBPrayerRequest.Id));
-		}
-	}
-}
+//		//DELETE
+//		public static async Task DeleteCosmosPrayerRequestsAsync(CosmosDBPrayerRequest deleteCosmosDBPrayerRequest)
+//		{
+//			await myDocumentClient.DeleteDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, deleteCosmosDBPrayerRequest.Id));
+//		}
+//	}
+//}
