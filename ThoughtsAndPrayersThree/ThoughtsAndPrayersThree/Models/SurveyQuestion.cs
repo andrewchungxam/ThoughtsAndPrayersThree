1634f543e2189a6df1cce0a7e93c470614744eba
ac <andrew.chung@xamarin.com>
2018-03-30 15:28:42 -0700

    Cleanup up
    Cleanup up

--- ThoughtsAndPrayersThree/ThoughtsAndPrayersThree/Models/SurveyQuestion.cs
+++ ThoughtsAndPrayersThree/ThoughtsAndPrayersThree/Models/SurveyQuestion.cs
@@ -1,34 +1,34 @@
-﻿using System;
-namespace ThoughtsAndPrayersThree.Models
-{
+﻿//using System;
+//namespace ThoughtsAndPrayersThree.Models
+//{
 
-//    [JsonObject(Title = "questions")]
-    public class SurveyQuestion
-    {
-        public string Id { get; set; }
-  //      [JsonProperty("text")]
-        public string Question { get; set; }
-        public string Answers { get; set; }
+////    [JsonObject(Title = "questions")]
+//    public class SurveyQuestion
+//    {
+//        public string Id { get; set; }
+//  //      [JsonProperty("text")]
+//        public string Question { get; set; }
+//        public string Answers { get; set; }
 
-        //added the fields
-        //public string 
+//        //added the fields
+//        //public string 
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string FullName { get; set; }
-        public string SharedText { get; set; }
+//        public string FirstName { get; set; }
+//        public string LastName { get; set; }
+//        public string FullName { get; set; }
+//        public string SharedText { get; set; }
 
-        public string NewText { get; set; }
-        public string FBProfileUrl { get; set; }
+//        public string NewText { get; set; }
+//        public string FBProfileUrl { get; set; }
 
-        public string CreateDateString { get; set; }
-
-
-        public override string ToString()
-        {
-            return Question;
-        }
-    }
+//        public string CreateDateString { get; set; }
 
 
-}
+//        public override string ToString()
+//        {
+//            return Question;
+//        }
+//    }
+
+
+//}
--- ThoughtsAndPrayersThree/ThoughtsAndPrayersThree/ThoughtsAndPrayersApp.cs
+++ ThoughtsAndPrayersThree/ThoughtsAndPrayersThree/ThoughtsAndPrayersApp.cs
@@ -26,123 +26,91 @@ namespace ThoughtsAndPrayersThree
 {
     public class App : Application
     {
-        //public static DogRepository DogRep { get; private set; }
-        //#TODO
-        //		public static DogPhotoRepository DogPhotoRep { get; set; }
-        //public static DogRepositoryBaseSixtyFour DogRepBaseSixtyFour { get; private set; }
-        //public static DogRepositoryBlob DogRepBlob { get; private set; }
-
-        //public static DogListMVVMPage MyDogListMVVMPage { get; set; }
-        //public static DogListPhotoPage MyDogListPhotoPage { get; set; }
-        //public static DogListPhotoBase64Page MyDogListPhotoBase64Page { get; set; }
-        //public static DogListPhotoBlobPage MyDogListPhotoBlobPage { get; set; }
-
-        //public static CloudBlobClient MyCloudBlobClient { get; set; }
-        //public static CloudBlobContainer MyCloudBlobContainer { get; set; }
-        //public static AzureBlobStorage MyAzureBlobStorage { get; set; }
-        //public static String ContainerName = "my10container";
-
         public static List<PrayerRequest> ListOfPrayers { get; set; } = FixedPrayerRequests.ListOfPrayerRequests;
-
-//        public static LocalData.PrayerRequestDatabase PrayerSQLDatabase { get; set; }
-
-        public static HttpClient myHttpClient;
 
         public App()
         {
 
+#region Global styles
 
- 
-
-			//HTTPClient
-			myHttpClient = new HttpClient();
-
-            ////MAKE THIS ASYNC AND PULL THIS OUT OF THE CONSTRUCTOR
-			//MyAzureBlobStorage = new AzureBlobStorage();
-			//MyCloudBlobClient = MyAzureBlobStorage.CreateCloudBlobClient();
-			//MyCloudBlobContainer = MyAzureBlobStorage.CreateCloudBlobClientAndContainer(ContainerName);
-
-//			string dbPath = LocalData.FileAccessHelper.GetLocalFilePath("ThoughtsAndPrayer1.db3");
-
-			////USE THIS FOR LIST PHOTO PAGE
-			//#TODO 
-//			DogPhotoRep = new DogPhotoRepository(dbPath);
-
-
-
-var labelStyle = new Style(typeof(Label))
+            var labelStyle = new Style(typeof(Label))
             {
 
-#if __ANDROID__
-                Setters = 
-                    {
-                        new Setter
-                        { 
-                            Property = Label.FontFamilyProperty,   Value = "Droid Sans Mono"
-                        }
-                    }
-                };
-#endif
+            #if __ANDROID__
+                            Setters = 
+                                {
+                                    new Setter
+                                    { 
+                                        Property = Label.FontFamilyProperty,   Value = "Droid Sans Mono"
+                                    }
+                                }
+                            };
+            #endif
 
-#if __IOS__
-                Setters =
-                    {
-                        new Setter
+            #if __IOS__
+                            Setters =
+                                {
+                                    new Setter
+                                    {
+                                        Property = Label.FontFamilyProperty,   Value = "AppleSDGothicNeo-Light"
+                                    }
+                                }
+                        };
+            #endif                        
+
+            #if __Windows__
+                        Setters = 
+                                {
+                                    new Setter
+                                    { 
+                                        Property = Label.FontFamilyProperty,   Value = "Times New Roman"
+                                    }
+                                }
+                            };
+            #endif
+                        var editorStyle = new Style(typeof(Editor))
                         {
-                            Property = Label.FontFamilyProperty,   Value = "AppleSDGothicNeo-Light"
-                        }
-                    }
-            };
-#endif                        
+            #if __ANDROID__
+                            Setters = 
+                                {
+                                    new Setter
+                                    { 
+                                        Property = Label.FontFamilyProperty,   Value = "Droid Sans Mono"
+                                    }
+                                }
+                            };
+            #endif
 
-#if __Windows__
-            Setters = 
-                    {
-                        new Setter
-                        { 
-                            Property = Label.FontFamilyProperty,   Value = "Times New Roman"
-                        }
-                    }
-                };
-#endif
-            var editorStyle = new Style(typeof(Editor))
-            {
-#if __ANDROID__
-                Setters = 
-                    {
-                        new Setter
-                        { 
-                            Property = Label.FontFamilyProperty,   Value = "Droid Sans Mono"
-                        }
-                    }
-                };
-#endif
+            #if __IOS__
+                            Setters =
+                                {
+                                new Setter
+                                {
+                                    Property = Label.FontFamilyProperty,
+                                    Value = "AppleSDGothicNeo-Light"
+                                }
+                                }
+                        };
+            #endif                        
 
-#if __IOS__
-                Setters =
-                    {
-                    new Setter
-                    {
-                        Property = Label.FontFamilyProperty,
-                        Value = "AppleSDGothicNeo-Light"
-                    }
-                    }
-            };
-#endif                        
+            #if __Windows__
+                        Setters = 
+                                {
+                                    new Setter
+                                    { 
+                                        Property = Label.FontFamilyProperty,   Value = "Times New Roman"
+                                    }
+                                }
+                            };
+            #endif
 
-#if __Windows__
-            Setters = 
-                    {
-                        new Setter
-                        { 
-                            Property = Label.FontFamilyProperty,   Value = "Times New Roman"
-                        }
-                    }
-                };
-#endif
+
+
             Resources = new ResourceDictionary();
             Resources.Add(labelStyle); //THIS IS IMPLICIT
             Resources.Add("editorStyle", editorStyle); //THIS IS IMPLICIT
+
+#endregion
 
 
 #if __ANDROID__
@@ -151,51 +119,17 @@ var labelStyle = new Style(typeof(Label))
 
 #if __IOS__
 
-            //MainPage = new ThoughtsAndPrayersThree.MainPage();
-            //MainPage = new ThoughtsAndPrayersThree.MainPage();
-
             var np = new NavigationPage(new PrayerListPage());
             np.BarBackgroundColor = MyColors.MyBlue1;
             np.BarTextColor = Color.White;
 
             MainPage = np;
-
-
-
-            //CONFIRMED WORKING
-            //MainPage = new TestAnimationPage();
-
 #endif
-
-			//ADDING STYLES TO RESOURCE DICTIONARIES
-//			Resources = new ResourceDictionary();
-			//IMPLICIT 
-//			Resources.Add(StyleDictionary.navigationPageStyle);
-
-			//ANDROID
-
-			//DogRepBlob = new DogRepositoryBlob(dbPath);
-
-
-			////Initialize Dog Photo View Page
-			//MyDogListMVVMPage = new DogListMVVMPage();
-			//MyDogListPhotoPage = new DogListPhotoPage();
-			//MyDogListPhotoBase64Page = new DogListPhotoBase64Page();
-			//MyDogListPhotoBlobPage = new DogListPhotoBlobPage();
-
-
-
-
-
 		}
-
 
 		protected override void OnStart()
 		{
-			//MobileCenter.Start(ASampleAppConstants.MobileCenterIOS +
-				   //"uwp={Your UWP App secret here};" +
-				   //"android={Your Android App secret here}",
-				   //typeof(Analytics), typeof(Crashes));
+
 		}
 
 		protected override void OnSleep()
@@ -209,114 +143,3 @@ var labelStyle = new Style(typeof(Label))
 		}
 	}
 }
-
-
-
-
-
-//using System;
-//using System.Linq;
-//using System.Net.Http;
-//using System.Threading.Tasks;
-//using System.Collections.Generic;
-
-//using Xamarin.Forms;
-
-//using Microsoft.Azure.Mobile;
-//using Microsoft.Azure.Mobile.Crashes;
-//using Microsoft.Azure.Mobile.Analytics;
-//using Microsoft.WindowsAzure.Storage.Blob;
-
-//using ASampleApp.Data;
-//using ASampleApp.Pages;
-//using ASampleApp.Models;
-//using ASampleApp.Constants;
-//using ASampleApp.BlobStorage;
-
-//namespace ASampleApp
-//{
-//    public class App : Application
-//    {
-
-////        public static DogRepository DogRep { get; private set; }
-//        public static DogPhotoRepository DogPhotoRep { get; set; }
-//        //public static DogRepositoryBaseSixtyFour DogRepBaseSixtyFour { get; private set; }
-//        public static DogRepositoryBlob DogRepBlob { get; private set; }
-
-//        public static DogListMVVMPage MyDogListMVVMPage { get; set; }
-//        public static DogListPhotoPage MyDogListPhotoPage { get; set; }
-//        public static DogListPhotoBase64Page MyDogListPhotoBase64Page  { get;set;}
-//        public static DogListPhotoBlobPage MyDogListPhotoBlobPage { get; set; }
-
-
-
-//        public static CloudBlobClient MyCloudBlobClient { get; set; }
-
-//        public static CloudBlobContainer MyCloudBlobContainer { get; set; }
-
-//        public static AzureBlobStorage MyAzureBlobStorage { get; set; }
-
-//        public static String ContainerName = "my10container";
-
-//        public static HttpClient myHttpClient;
-
-//        public App ()
-//        {
-//            //HTTPClient
-//            myHttpClient = new HttpClient();
-
-//            //MAKE THIS ASYNC AND PULL THIS OUT OF THE CONSTRUCTOR
-//            MyAzureBlobStorage = new AzureBlobStorage();
-//            MyCloudBlobClient = MyAzureBlobStorage.CreateCloudBlobClient();
-//            MyCloudBlobContainer = MyAzureBlobStorage.CreateCloudBlobClientAndContainer(ContainerName);
-
-//            string dbPath = FileAccessHelper.GetLocalFilePath("asa17dog12.db3");
-//            //USE THIS FOR LIST PAGE
-//            //DogRep = new DogRepository(dbPath);
-//            ////USE THIS FOR LIST PHOTO PAGE
-//            //DogPhotoRep = new DogPhotoRepository(dbPath);
-//            ////USE THIS FOR LIST PHOTO BASE SIXTY FOUR PAGE
-//            //DogRepBaseSixtyFour = new DogRepositoryBaseSixtyFour(dbPath);
-
-//            DogRepBlob = new DogRepositoryBlob(dbPath);
-
-//            var applicationStartPage = new FirstPage ();
-
-//            var myNavigationPage = new NavigationPage(applicationStartPage);
-
-//            MainPage = myNavigationPage;
-
-//            //Initialize Dog Photo View Page
-//            MyDogListMVVMPage = new DogListMVVMPage();
-//            MyDogListPhotoPage = new DogListPhotoPage();
-//            MyDogListPhotoBase64Page = new DogListPhotoBase64Page();
-//            MyDogListPhotoBlobPage = new DogListPhotoBlobPage();
-
-//        }
-
-
-//        protected override void OnStart ()
-//        {
-//            // Handle when your app starts
-//            //MobileCenter.Start("ios=294968e2-d712-43fc-a8bd-1492ed9ea29c;" +
-//            //"uwp={Your UWP App secret here};" +
-//            //"android={Your Android App secret here}",
-//            //typeof(Analytics), typeof(Crashes));
-
-//            MobileCenter.Start(ASampleAppConstants.MobileCenterIOS +
-//                   "uwp={Your UWP App secret here};" +
-//                   "android={Your Android App secret here}",
-//                   typeof(Analytics), typeof(Crashes));
-//        }
-
-//        protected override void OnSleep ()
-//        {
-//            // Handle when your app sleeps
-//        }
-
-//        protected override void OnResume ()
-//        {
-//            // Handle when your app resumes
-//        }
-//    }
-//}
