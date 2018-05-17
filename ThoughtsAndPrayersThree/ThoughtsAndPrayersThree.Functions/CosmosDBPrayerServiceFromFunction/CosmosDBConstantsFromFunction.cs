f774ece9180b0fed590b1b54fe96ab5135bfcde8
andrewchungxam <andrew.chung@xamarin.com>
2018-05-17 12:26:59 -0700

    Added functions from Windows Machine
    Added functions from Windows Machine

---
+++ ThoughtsAndPrayersThree/ThoughtsAndPrayersThree.Functions/CosmosDBPrayerServiceFromFunction/CosmosDBConstantsFromFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThoughtsAndPrayersThree.Functions.CosmosDB
+{
+    public static class CosmosDBConstantsFromFunctions
+    {
+        //#error SIGN UP FOR AZURE - THEN SET THE BELOW KEYS AND THEN COMMENT OUT THIS LINE
+        //public static readonly String myEndPoint = "https://YOUR_AZURE_COSMOSDB_NAME.documents.azure.com:443/";
+        //public static readonly String myKey = "YOUR_SECRET_KEY_23j9fj932jrh23hr93r29jrj3r2j3rjjdf==";
+        public static readonly String myEndPoint = "https://thoughtandprayers.documents.azure.com:443/";
+        public static readonly String myKey = "0skBTr3vdf62xx7iWaKEJEGQbDHTouaoIYhj6ZGX51ZT3tfITlbDRUUVCRLsmUy81IgoBHvq2lyc69G3Fbt2XA==";
+        //#error Add the following Nuget package to all your platform projects :: Microsoft.Azure.DocumentDb.Core
+    }
+}
